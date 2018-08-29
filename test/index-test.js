a05a1341b6d0d6ba5623ca52890422aedc630d4b
Drake Bott <bott.da@gmail.com>
2018-08-29 09:32:34 -0600

    Create index test for testing dictionary populate

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,17 @@
- import { assert } from 'chai';
+import { assert } from 'chai';
+import Trie from '../lib/Trie';
+import fs from 'fs';
 
- describe('test', function() {
-   it('should return true', function() {
-     assert.equal(true, true);
+ describe('Should create a trie andi import the dictionary', () =>  {
+   it('should create a trie', () => {
+     let trie = new Trie();
+     assert.isObject(trie);
+   });
+   it('Should populate with the dictionary', () => {
+     const trie = new Trie();
+     const text = "/usr/share/dict/words";
+     const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+     trie.populate(dictionary);
+     assert.equal(trie.count, dictionary.length);
    });
  });
